9dbcc59c98844a455c0f3fc82edd2f80fd1d5a9f
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-13 15:09:04 +0000

    Update sft.sh

--- sft.sh
+++ sft.sh
@@ -5,4 +5,4 @@ source /mnt/fast/nobackup/users/nt00601/miniconda3/etc/profile.d/conda.sh
 conda activate handbook
 
 # standard template: ACCELERATE_LOG_LEVEL=info accelerate launch --config_file path/to/multi_gpu.yaml --num_processes={num_gpus} path/to/scripts/run_sft.py path/to/config_qlora.yaml
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/recipes/accelerate_configs/multi_gpu.yaml --num_processes=8 /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/scripts/run_sft.py /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/mamba-sft-qlora.yaml
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/recipes/accelerate_configs/multi_gpu.yaml --num_processes=1 /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/scripts/run_sft.py /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/mamba-sft-qlora.yaml
