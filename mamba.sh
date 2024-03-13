7523d62c24c56bc886c0e008192794b4dee00355
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-13 23:00:36 +0000

    Update mamba.sh

--- mamba.sh
+++ mamba.sh
@@ -5,6 +5,9 @@ source /mnt/fast/nobackup/users/nt00601/miniconda3/etc/profile.d/conda.sh
 conda activate handbook
 
 # For mamba - Sort out undefined-symbol issue https://github.com/state-spaces/mamba/issues/169
-pip install https://github.com/state-spaces/mamba/releases/download/causal_conv1d-1.1.1+cu118torch2.1cxx11abiFALSE-cp310-cp310-linux_x64_64.whl
-pip install https://github.com/state-spaces/mamba/releases/download/mamba_ssm-1.1.2+cu118torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+# mamba only supports CUDA 11.8 and CUDA 12.2, so pick according to what your condor have
+# cp310 = python version 3.10
+# torch 2.1 = Pytorch 2.10
+pip install https://github.com/state-spaces/mamba/releases/download/causal_conv1d-1.1.1+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x64_64.whl
+pip install https://github.com/state-spaces/mamba/releases/download/mamba_ssm-1.1.2+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
