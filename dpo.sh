86842b7f51a29a9fbc83e955a3c6278bbb3bf86e
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-13 14:46:38 +0000

    Create dpo.sh

---
+++ dpo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Not recommend to source .bashrc in HPC condor
+source /mnt/fast/nobackup/users/nt00601/miniconda3/etc/profile.d/conda.sh
+conda activate handbook
+
+# standard template: ACCELERATE_LOG_LEVEL=info accelerate launch --config_file path/to/multi_gpu.yaml --num_processes={num_gpus} path/to/scripts/run_sft.py path/to/config_qlora.yaml
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/recipes/accelerate_configs/multi_gpu.yaml --num_processes=8 /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/scripts/run_dpo.py /mnt/fast/nobackup/users/nt00601/mamba-alignment-handbook/mamba-sft-qlora.yaml
