2200888406821baf80f33d16af94a60a91b6dce1
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-13 20:33:44 +0000

    Update mamba.sh

--- mamba.sh
+++ mamba.sh
@@ -4,7 +4,7 @@
 source /mnt/fast/nobackup/users/nt00601/miniconda3/etc/profile.d/conda.sh
 conda activate handbook
 
-# For mamba
+# For mamba - Sort out undefined-symbol issue https://github.com/state-spaces/mamba/issues/169
 pip install causal_conv1d-1.0.1+cu188torch2.1cxx11abiFALSE-cp310-cp310-linux_x64_64.whl
 pip install mamba_ssm-1.0.1+cu118torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
