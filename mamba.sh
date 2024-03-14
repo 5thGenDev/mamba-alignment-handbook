884e9edbbd1990e4594831680a6b5d891524dcfe
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2024-03-14 09:38:32 +0000

    Update mamba.sh

--- mamba.sh
+++ mamba.sh
@@ -9,6 +9,6 @@ conda activate handbook
 # cp310 = python version 3.10
 # torch 2.1 = Pytorch 2.10
 # Have a look at this and google which tag is compatible with CUDA 11.8 or CUDA 12.2
-pip install https://github.com/state-spaces/mamba/releases/download/causal_conv1d-1.1.1+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x64_64.whl
-pip install https://github.com/state-spaces/mamba/releases/download/mamba_ssm-1.1.1+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+pip install https://github.com/state-spaces/mamba/releases/causal_conv1d-1.1.1+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x64_64.whl
+pip install https://github.com/state-spaces/mamba/releases/mamba_ssm-1.1.1+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
